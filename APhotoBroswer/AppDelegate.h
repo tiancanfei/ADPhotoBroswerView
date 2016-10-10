75af0d081906b8c9bdad8cba6ee806254409542e
安德航 <bjwltiankong@163.com>
2016-10-10 10:30:52 +0800

    Initial Commit

---
+++ APhotoBroswer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  APhotoBroswer
+//
+//  Created by andehang on 16/10/10.
+//  Copyright © 2016年 andehang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
